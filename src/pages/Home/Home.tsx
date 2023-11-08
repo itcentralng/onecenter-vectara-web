aa771f76c5bf8b196a57070ec6d9af28900b24f6
Ibrahim <i.aliyu019@gmail.com>
2023-11-08 21:52:52 +0100

    getpartners immediately when user views the homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Footer from "../../components/Footer";
 import HeroSection from "./HeroSection";
+import { getPartners } from "../../slices/PartnerSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store";
 
 function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(getPartners());
+  }, []);
   return (
     <div>
       <HeroSection />
